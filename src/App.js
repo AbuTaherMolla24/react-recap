f28570255ff7d298ccbcca504053f34c750c07f8
Abu Taher Molla <57549941+AbuTaherMolla24@users.noreply.github.com>
2020-08-22 18:14:49 +0600

    33.7 end

--- src/App.js
+++ src/App.js
@@ -3,24 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks = ['tuna','salman', 'kawser']
   return (
     <div className="App">
+      <Nayok name={nayoks[0]}  age='5'></Nayok>
+      <Nayok name={nayoks[1]}  age=''></Nayok>
+      <Nayok name={nayoks[2]}  age=''></Nayok>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
+}
+function Nayok(props) {
+  const nayokStyle = {
+    border: '3px solid gray',
+    margin: '20px'
+  }
+  return(
+    <div style={nayokStyle}>
+      <h1>ami tumar chilam na!{props.name} </h1>
+      <h3>cheka khaichi yars  {props.age || '6'}</h3>
+    </div>
+  )
 }
 
 export default App;
